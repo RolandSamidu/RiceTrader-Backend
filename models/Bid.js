6f76d7e4c4e0828325dcb8641fbbaea092fe3c03
RolandSamidu <samidu.appuhamy@gmail.com>
2025-03-16 20:37:08 +0530

    updated bid model and route

--- models/Bid.js
+++ models/Bid.js
@@ -4,7 +4,7 @@ const BidSchema = new mongoose.Schema({
     post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
     bidder: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     amount: { type: Number, required: true },
-    status: { type: String, enum: ['Pending', 'Accepted', 'Rejected'], default: 'Pending' }
+    status: { type: String, enum: ['Pending', 'Accepted', 'Completed', 'Rejected'], default: 'Pending' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Bid', BidSchema);
