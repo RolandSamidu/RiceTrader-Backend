1963489cb191f30e71809f28a47ee82eeb5fabd1
RolandSamidu <samidu.appuhamy@gmail.com>
2025-03-16 20:23:53 +0530

    Added notification model and updated payment model

--- models/Payment.js
+++ models/Payment.js
@@ -4,7 +4,9 @@ const PaymentSchema = new mongoose.Schema({
     bid: { type: mongoose.Schema.Types.ObjectId, ref: 'Bid', required: true },
     payer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     amount: { type: Number, required: true },
-    status: { type: String, enum: ['Pending', 'Completed'], default: 'Pending' }
+    status: { type: String, enum: ['Pending', 'Completed'], default: 'Pending' },
+    transactionId: { type: String, required: true },
+    paymentMethod: { type: String, enum: ['Card', 'Bank Transfer'], required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Payment', PaymentSchema);
