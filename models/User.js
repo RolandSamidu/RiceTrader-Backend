072900025107846ecbff8bce503d86807ecd84ae
RolandSamidu <samidu.appuhamy@gmail.com>
2025-03-25 18:53:21 +0530

    Updated

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['Farmer', 'Intermediate', 'Rice Producer'], required: true },
-    profilePicture: { type: String } // URL of the uploaded image
+    profilePicture: { type: String, default: "/uploads/default.png" }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
